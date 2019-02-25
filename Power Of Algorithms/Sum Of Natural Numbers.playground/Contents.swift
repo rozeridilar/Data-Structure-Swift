d465e5d69efc6d48d18bd6ec79ef2235241b9da8
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-02-25 09:36:58 +0300

    Find Two Sum

---
+++ Power Of Algorithms/Sum Of Natural Numbers.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+
+//brute-force approach
+//linear time complexity
+//O(n)
+func sum (_ n:UInt) -> UInt{
+    var result: UInt = 0
+    
+    for i in 1...n{
+        result += i
+    }
+    
+    return result
+}
+
+//optimized in Constant Time
+//O(1)
+func sumOptimized(_ n: UInt) -> UInt{
+    return n * (n+1) / 2
+}
