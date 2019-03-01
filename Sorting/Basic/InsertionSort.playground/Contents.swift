93a1f102c166a939e05b080bb02ac7cb8ae5713e
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-03-01 13:32:41 +0300

    insertion_sort

---
+++ Sorting/Basic/InsertionSort.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+func insertionSort(_ input:[Int]) -> [Int]{
+    guard input.count > 1 else{
+        return input
+    }
+    
+    var result = input
+    let count = result.count
+    
+    for sortedIndex in 0..<count{
+        var backIndex = sortedIndex
+        
+        while backIndex > 0 && result[backIndex] < result[backIndex - 1] {
+            result.swapAt(backIndex-1, backIndex)
+            backIndex -= 1
+        }
+    }
+    return result
+}
+
+print(insertionSort([1,2,3,5,4]))
+
