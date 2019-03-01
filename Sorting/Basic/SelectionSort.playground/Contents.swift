ed0aa97a6596581d5eca8418b66ebf21c4826c92
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-03-01 08:54:17 +0300

    sorting_

--- Sorting/Basic/SelectionSort.playground/Contents.swift
+++ Sorting/Basic/SelectionSort.playground/Contents.swift
@@ -1,3 +1,29 @@
-import Cocoa
+import Foundation
 
-var str = "Hello, playground"
+//It starts by the finding the smallest item and exchanging it with the first one. Then, it finds the second smalest item then exchanges it with the second item. List goes on like this until the entire sequence s sorted.
+
+func selectionSort(_ input:[Int]) -> [Int]{
+    guard input.count > 1 else{
+        return input
+    }
+    
+    var result = input
+    
+    for index in 0..<(result.count - 1){
+        var indexLowest = index
+        
+        for forwardIndex in (index + 1)..<result.count{
+            if result[forwardIndex] < result[indexLowest]{
+                indexLowest = forwardIndex
+            }
+        }
+        
+        if index != indexLowest{
+            result.swapAt(index, indexLowest)
+        }
+    }
+    
+    return result
+}
+
+print(selectionSort([1,2,3,5,4]))
