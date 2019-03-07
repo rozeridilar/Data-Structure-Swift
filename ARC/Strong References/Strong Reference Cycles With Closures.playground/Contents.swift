474adc69518f41942b0a169603798f93a4e98a4b
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-03-07 11:29:57 +0300

    ARC_Closures

--- ARC/Strong References/Strong Reference Cycles With Closures.playground/Contents.swift
+++ ARC/Strong References/Strong Reference Cycles With Closures.playground/Contents.swift
@@ -4,6 +4,18 @@ public class Contact{
     let name: String
     let phoneNumber: String?
     
+    public lazy var prettyPrint: () -> String = { [weak self] in
+        if let strongSelf = self{
+            var result = strongSelf.name
+            if let phoneNumber = strongSelf.phoneNumber{
+                result = "Name: \t \(result) PhoneNumber: \t \(phoneNumber)"
+            }
+            return result
+        }else{
+            return ""
+        }
+    }
+    
     public init (name: String, phoneNumber: String? = nil){
         self.name = name
         self.phoneNumber = phoneNumber
@@ -15,5 +27,6 @@ public class Contact{
     }
 }
 
-var johnDoe: Contact? = Contact(name: "johnDoe")
+var johnDoe: Contact? = Contact(name: "johnDoe", phoneNumber: "+1 555 555")
+print(johnDoe!.prettyPrint())
 johnDoe = nil
