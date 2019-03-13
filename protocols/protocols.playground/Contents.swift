f336328726da798225abe132b60c14719ea23c18
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-03-13 13:40:38 +0300

    s_protocol

---
+++ protocols/protocols.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+enum Format {
+    case XML
+    case JSON
+}
+
+protocol Representable{
+    func represantation( asType: Format ) -> String
+    init(asType: Format, contents: String)
+}
+
+class Employee : Representable {
+    
+    private var name: String = "Rozeri"
+    required init(asType: Format, contents: String) {
+        /**/
+    }
+    init(name: String){
+        self.name = name
+    }
+    func represantation(asType: Format) -> String {
+        switch asType {
+        case .XML:
+            return "<Employee name = \(name)>"
+        case .JSON:
+            return "{Employee: {name: \(name)}}"
+        }
+    }
+    
+}
+
+print(Employee(name: "Rozeri").represantation(asType: .JSON))
