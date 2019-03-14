8e008697afd5516d3ae605d0925ef0e64e877392
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-03-14 10:39:17 +0300

    Hash Tables: Ransom Note

--- S/Hash Tables- Ransom Note.playground/Contents.swift
+++ S/Hash Tables- Ransom Note.playground/Contents.swift
@@ -1,3 +1,37 @@
-import Cocoa
+import Foundation
 
-var str = "Hello, playground"
+//09:42 am
+
+// Complete the checkMagazine function below.
+func checkMagazine(magazine: [String], note: [String]) -> Void {
+    var result = ""
+    var mag = magazine
+    let mappedItems = note.map{($0,1)}
+    let counts = Dictionary(mappedItems, uniquingKeysWith: +)
+    for dic in counts{
+        if !magazine.contains(dic.key){
+            print("No")
+            return
+        }
+        if dic.value > 1 {
+            for i in 0..<dic.value{
+                let firstCount = mag.count
+                mag = mag.filter { $0 != dic.key}
+                let lastCount = mag.count
+                if firstCount - lastCount != dic.value{
+                    result = "No"
+                    print(result)
+                    return
+                }
+            }
+        }else{
+            //print(dic.key)
+            mag = mag.filter{($0 != dic.key)}
+            // print(mag)
+        }
+        
+    }
+   
+     print ("\(mag.count >= 0 ? "Yes" : "No")")
+}
+print (checkMagazine(magazine: ["ive", "got","a", "lovely", "bunch", "of","coconuts"], note: ["Ive", "got","some","coconuts"]))
