2806e00441c10ac4f876567e2fbb3165a6d8a128
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-04-02 08:41:33 +0300

    base_project

---
+++ notification&observers/notification&observers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notification&observers
+//
+//  Created by Kızılay on 2.04.2019.
+//  Copyright © 2019 Turk Kizilay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
