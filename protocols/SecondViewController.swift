4526235d353be916a04acdae13e8443e2462e042
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-04-02 08:49:17 +0300

    base_project

---
+++ protocols/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  notification&observers
+//
+//  Created by Kızılay on 2.04.2019.
+//  Copyright © 2019 Turk Kizilay. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
