33fb63d35df11e6c5921e310f53d440b2ff96806
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-04-02 10:24:33 +0300

    protocols

--- notification&observers/notification&observers/ViewController.swift
+++ notification&observers/notification&observers/ViewController.swift
@@ -9,8 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    
     //MARK: Outlets
     @IBOutlet weak var mainImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
@@ -27,9 +25,17 @@ class ViewController: UIViewController {
 
     //MARK: Actions
     @IBAction func chooseButtonTapped(_ sender: Any) {
-        let secondVC = storyboard?.instantiateViewController(withIdentifier: "secondVC")
-        present(secondVC!, animated: true, completion: nil)
+        let secondVC = storyboard?.instantiateViewController(withIdentifier: "secondVC") as! SecondViewController
+        secondVC.selectionDelegate = self
+        present(secondVC, animated: true, completion: nil)
     }
     
+}
+extension ViewController: SideSelectionDelegate{
+    func didTapChoice(image: UIImage, name: String, color: UIColor) {
+        mainImageView.image = image
+        nameLabel.text = name
+        self.view.backgroundColor = color
+    }
 }
 
--- protocols/SecondViewController.swift
+++ protocols/SecondViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
+protocol SideSelectionDelegate {
+    func didTapChoice(image: UIImage, name: String, color: UIColor)
+}
+
 class SecondViewController: UIViewController {
 
+var selectionDelegate: SideSelectionDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,9 +25,11 @@ class SecondViewController: UIViewController {
     @IBAction func buttonTapped(_ sender: UIButton) {
         switch sender.tag {
         case 1:
+            selectionDelegate.didTapChoice(image: UIImage(named: "vader")!, name: "Darth Vader", color: .red)
             dismiss(animated: true, completion: nil)
             break
         case 2:
+            selectionDelegate.didTapChoice(image: UIImage(named: "luke")!, name: "Luke Skywalker", color: .cyan)
             dismiss(animated: true, completion: nil)
             break
         default:
